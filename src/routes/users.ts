640df97131e96bff6e25020ea9f65c3613e16a5d
Xuanmi Deploy <deploy@xuanmi.net>
2026-07-03 12:27:03 +0800

    Fix: move @types to dependencies + add type annotations to users.ts (fix Railway build)

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,7 +1,7 @@
 /**
  * 用户管理路由
  */
-import { Router } from 'express'
+import { Router, Request, Response, NextFunction } from 'express'
 import { userController } from '../controllers/user.controller'
 import { authenticate, requireAdmin } from '../middleware/auth'
 import { validate } from '../middleware/validate'
@@ -12,11 +12,11 @@ const router = Router()
 router.use(authenticate)
 router.use(requireAdmin)
 
-router.get('/', validate(userQuerySchema, 'query'), (req, res, next) => userController.list(req, res, next))
-router.get('/:id', (req, res, next) => userController.getById(req, res, next))
-router.post('/', validate(createUserSchema), (req, res, next) => userController.create(req, res, next))
-router.put('/:id', validate(updateUserSchema), (req, res, next) => userController.update(req, res, next))
-router.delete('/:id', (req, res, next) => userController.delete(req, res, next))
-router.patch('/:id/status', validate(updateUserStatusSchema), (req, res, next) => userController.updateStatus(req, res, next))
+router.get('/', validate(userQuerySchema, 'query'), (req: Request, res: Response, next: NextFunction) => userController.list(req, res, next))
+router.get('/:id', (req: Request, res: Response, next: NextFunction) => userController.getById(req, res, next))
+router.post('/', validate(createUserSchema), (req: Request, res: Response, next: NextFunction) => userController.create(req, res, next))
+router.put('/:id', validate(updateUserSchema), (req: Request, res: Response, next: NextFunction) => userController.update(req, res, next))
+router.delete('/:id', (req: Request, res: Response, next: NextFunction) => userController.delete(req, res, next))
+router.patch('/:id/status', validate(updateUserStatusSchema), (req: Request, res: Response, next: NextFunction) => userController.updateStatus(req, res, next))
 
 export default router
